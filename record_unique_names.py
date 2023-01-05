79408a9f2a2fcefea9c0dd06d1f12b17f53b22cf
RMANOV <96174405+RMANOV@users.noreply.github.com>
2023-01-05 13:52:22 +0200

    13.52

---
+++ record_unique_names.py
@@ -0,0 +1,14 @@
+# Record Unique Names
+# Write a program that will take a list of names and print only the unique names in the list.
+# The order in which we print the result does not matter.
+
+number_of_names = int(input())
+names_set = set()
+
+for i in range(number_of_names):
+    name = input()
+    names_set.add(name)
+
+unique_names = list(names_set)
+print(*unique_names, sep ="")
+
