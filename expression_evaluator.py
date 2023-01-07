c7045e078bb33862317702f0dcfea10520bdd60d
RMANOV <96174405+RMANOV@users.noreply.github.com>
2023-01-07 16:41:32 +0200

    16.42

--- expression_evaluator.py
+++ expression_evaluator.py
@@ -21,32 +21,119 @@
 # •	There will be no case of division by zero
 # •	There might be negative numbers in the string
 
-expression = input().split()
-result = 0
 
-for i in range(len(expression)):
-    if expression[i] == "*":
-        result = int(expression[i-2]) * int(expression[i-1])
-        expression[i-2] = result
-        expression.pop(i)
-        expression.pop(i-1)
+expression = input().split()
+
+# Use a stack to store the numbers and operators
+stack = []
+
+# Precedence levels for the operators
+precedence = {'*': 2, '/': 2, '+': 1, '-': 1}
+
+for token in expression:
+    if token in precedence:
+        # Process the operators in the stack according to their precedence
+        while stack and stack[-1] in precedence and precedence[stack[-1]] >= precedence[token]:
+            op = stack.pop()
+            if len(stack) < 2:
+                # Not enough elements in the stack to perform the operation
+                break
+            num2 = stack.pop()
+            num1 = stack.pop()
+            if op == "*":
+                result = num1 * num2
+            elif op == "/":
+                result = num1 // num2
+            elif op == "+":
+                result = num1 + num2
+            elif op == "-":
+                result = num1 - num2
+            stack.append(result)
+        stack.append(token)
+    else:
+        # Token is a number, just push it to the stack
+        stack.append(int(token))
+
+# Process the remaining operators in the stack
+while len(stack) > 1:
+    if len(stack) < 3:
+        # Not enough elements in the stack to perform the operation
         break
-    elif expression[i] == "/":
-        result = int(expression[i-2]) / int(expression[i-1])
-        expression[i-2] = result
-        expression.pop(i)
-        expression.pop(i-1)
-        break
-    elif expression[i] == "+":
-        result = int(expression[i-2]) + int(expression[i-1])
-        expression[i-2] = result
-        expression.pop(i)
-        expression.pop(i-1)
-        break
-    elif expression[i] == "-":
-        result = int(expression[i-2]) - int(expression[i-1])
-        expression[i-2] = result
-        expression.pop(i)
-        expression.pop(i-1)
-        break
-print(int(result))
+    num2 = stack.pop()
+    num1 = stack.pop()
+    op = stack.pop()
+    if op == "*":
+        result = num1 * num2
+    elif op == "/":
+        result = num1 // num2
+    elif op == "+":
+        result = num1 + num2
+    elif op == "-":
+        result = num1 - num2
+    stack.append(result)
+
+# The final result should be the only element in the stack
+print(stack[0])
+
+
+
+
+# expression = input().split()
+# result = 0
+
+# # Use a stack to store the numbers
+# stack = []
+
+# for token in expression:
+#     if token in {"*", "/", "+", "-"}:
+#         # Pop the last two numbers from the stack
+#         num2 = stack.pop()
+#         num1 = stack.pop()
+#         if token == "*":
+#             result = num1 * num2
+#         elif token == "/":
+#             result = num1 // num2
+            
+#         elif token == "+":
+#             result = num1 + num2
+#         elif token == "-":
+#             result = num1 - num2
+#         # Push the result back to the stack
+#         stack.append(result)
+#     else:
+#         # Token is a number, just push it to the stack
+#         stack.append(int(token))
+
+# # The final result should be the only element in the stack
+# print(stack[0])
+
+
+# expression = input().split()
+# result = 0
+
+# for i in range(len(expression)):
+#     if expression[i] == "*":
+#         result = int(expression[i-2]) * int(expression[i-1])
+#         expression[i-2] = result
+#         expression.pop(i)
+#         expression.pop(i-1)
+#         break
+#     elif expression[i] == "/":
+#         result = int(expression[i-2]) / int(expression[i-1])
+#         expression[i-2] = round(result)
+#         expression.pop(i)
+#         expression.pop(i-1)
+#         break
+#     elif expression[i] == "+":
+#         result = int(expression[i-2]) + int(expression[i-1])
+#         expression[i-2] = result
+#         expression.pop(i)
+#         expression.pop(i-1)
+#         break
+#     elif expression[i] == "-":
+#         result = int(expression[i-2]) - int(expression[i-1])
+#         expression[i-2] = result
+#         expression.pop(i)
+#         expression.pop(i-1)
+#         break
+# print(int(result))
