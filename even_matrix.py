ff1baf03836d765b1415e385791a1f60bdab839d
RMANOV <96174405+RMANOV@users.noreply.github.com>
2023-01-14 15:50:28 +0200

    15.50

--- even_matrix.py
+++ even_matrix.py
@@ -9,4 +9,24 @@ row_count = int(input())
 for row in range(row_count):
     matrix = [int(x) for x in input().split(", ")]
     even_matrix = [[x for x in matrix if x % 2 == 0]]
-    print(*even_matrix, sep = ", ")
+    # print(*even_matrix, sep = ", ")
+    # print(*[even_matrix], sep = ", ")
+    # if row == 0:
+    #     print('[', end = "")
+    #     print(', '.join(map(str, even_matrix)), end = ", ")
+    # else:
+    #     print(', '.join(map(str, even_matrix)), end = ", ")
+    # if row == row_count - 2:
+    #     print(', '.join(map(str, even_matrix)), end = "")
+    # if row == row_count - 1:
+    #     print(']')
+
+even_matrix = [[x for x in row if x % 2 == 0] for row in matrix]
+print(',\n'.join([', '.join(map(str, row)) for row in even_matrix]))
+
+
+
+
+# print(', '.join(map(str, even_matrix)))
+# print(*[even_matrix], sep = ", ")
+# print(', '.join(map(str, even_matrix)))
