abd4fc4f8289a47cfb0006e2e5359f0fc855e699
RMANOV <96174405+RMANOV@users.noreply.github.com>
2023-01-17 11:49:55 +0200

    11.50

--- matrix_modification.py
+++ matrix_modification.py
@@ -9,25 +9,57 @@
 # A coordinate is valid if both of the given indexes are in range [0; len() – 1].
 # When you receive "END", you should print the matrix and stop the program.
 
-rows_columns = [int(x) for x in input().split()]
-matrix = [[int(x) for x in input().split()] for _ in range(rows_columns[0])]
+
+# Matrix Modification
+
+# Get matrix size
+rows = int(input())
+
+# Create matrix
+matrix = [[int(x) for x in input().split()] for _ in range(rows)]
 
 while True:
-    command = input()
-    if command == "END":
+    command = input().split()
+    if command[0] == "END":
         break
-    command, row, col, value = command.split()
+    command, row, col, value = command
     row, col, value = int(row), int(col), int(value)
-    # if row not in range(rows_columns[0]) or col not in range(rows_columns[1]) or value < 0 or value > 100:
-    if row in range(0, len(matrix)-1) and col in range(0, len(matrix[0])-1):
+    
+    # Check if coordinates are valid
+    if not (0 <= row < rows) or not (0 <= col < len(matrix[row])):
         print("Invalid coordinates")
         continue
+    
+    # Modify matrix values
     if command == "Add":
         matrix[row][col] += value
     elif command == "Subtract":
         matrix[row][col] -= value
 
-print(*[x for x in matrix], sep=" ")
+# Print matrix
+for row in matrix:
+    print(*row)
+
+
+# rows_columns = [int(x) for x in input().split()]
+# matrix = [[int(x) for x in input().split()] for _ in range(rows_columns[0])]
+
+# while True:
+#     command = input()
+#     if command == "END":
+#         break
+#     command, row, col, value = command.split()
+#     row, col, value = int(row), int(col), int(value)
+#     # if row not in range(rows_columns[0]) or col not in range(rows_columns[1]) or value < 0 or value > 100:
+#     if row in range(0, len(matrix)-1) and col in range(0, len(matrix[0])-1):
+#         print("Invalid coordinates")
+#         continue
+#     if command == "Add":
+#         matrix[row][col] += value
+#     elif command == "Subtract":
+#         matrix[row][col] -= value
+
+# print(*[x for x in matrix], sep=" ")
 
 
 # while True:
