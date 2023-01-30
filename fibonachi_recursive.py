c14813c8a9fa95cbe0cb4e6485a5436336b149ce
RMANOV <96174405+RMANOV@users.noreply.github.com>
2023-01-30 09:43:39 +0200

    09.43

--- fibonachi_recursive.py
+++ fibonachi_recursive.py
@@ -4,9 +4,8 @@
 def fib(n):
     if n <= 1:
         return n
-    else:
-        return fib(n - 1) + fib(n - 2)
-
+    return fib(n - 1) + fib(n - 2)
 
 n = int(input("Enter a number: "))
-print(fib(n))
+
+print(* [fib(i) for i in range(n)], sep = ", ")
