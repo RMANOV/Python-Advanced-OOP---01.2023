973079db68f0913956d718ec18e250f29cea9c04
RMANOV <96174405+RMANOV@users.noreply.github.com>
2023-02-08 12:51:57 +0200

    12.51

---
+++ multiplication_function.py
@@ -0,0 +1,11 @@
+# 1.	Multiplication Function
+# Write a function called multiply that can receive any quantity of numbers (integers) as different parameters and returns the result of the multiplication of all of them. 
+# Submit only your function in the Judge system.
+
+def multiply(*args):
+    result = 1
+    for num in args:
+        result *= num
+    return result
+
+multiply(int(x) for x in input().split(", "))
