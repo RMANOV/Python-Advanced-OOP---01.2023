6eae13b6cf7ed547aa94afd527737e9277d0fdd6
RMANOV <96174405+RMANOV@users.noreply.github.com>
2023-02-12 17:08:18 +0200

    17.08

---
+++ keyword_arguments_length.py
@@ -0,0 +1,11 @@
+# Keyword Arguments Length
+# Create a function called kwargs_length() that can receive some keyword arguments and return their length.
+
+def kwargs_length(**kwargs):
+    return len(kwargs)
+
+def main():
+    print(kwargs_lenght(a=1, b=2, c=3, d=4))
+
+if __name__ == "__main__":
+    main()
