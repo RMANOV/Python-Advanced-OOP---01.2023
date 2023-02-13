1da75ebf245d20f0ab498ccc0a0e03eea4413c7d
RMANOV <96174405+RMANOV@users.noreply.github.com>
2023-02-13 12:48:45 +0200

    12.48

--- function_executor.py
+++ function_executor.py
@@ -1,7 +1,7 @@
 # Function Executor
-# Create a function called func_executor() that can receive a different number of tuples, 
+# Create a function called func_executor() that can receive a different number of tuples,
 # each of which will have exactly 2 elements: the first will be a function, 
-# and the second will be a tuple of the arguments that need to be passed to that function. 
+# and the second will be a tuple of the arguments that need to be passed to that function.
 # You should execute each function and return its result in the format:
 # "{function name} - {function result}"
 
